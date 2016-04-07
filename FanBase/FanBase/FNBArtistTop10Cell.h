9f7cbbbbe82c4d55cb8f76836b8f5b3b506d1686
RodAquino <rodrigo_59@hotmail.com>
2016-04-07 14:22:57 -0400

    added files to master

---
+++ FanBase/FanBase/FNBArtistTop10Cell.h
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.h
+//  spotifytop10
+//
+//  Created by Rodrigo on 4/1/16.
+//  Copyright © 2016 Rodrigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FNBArtistTop10Cell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *AlbumImageView;
+
+@property (weak, nonatomic) IBOutlet UILabel *TrackNameLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *AlbumNameLabel;
+
+@end
