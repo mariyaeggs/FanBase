d1ab5ed0f8f00a9aaa2a7bd96c022152e8486141
andynovak12 <andynovak12@gmail.com>
2016-04-07 18:07:10 -0400

    Started working on artist page

---
+++ FanBase/FanBase/ArtistMainPageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ArtistMainPageTableViewController.h
+//  FanBase
+//
+//  Created by Andy Novak on 4/7/16.
+//  Copyright © 2016 Angelica Bato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArtistMainPageTableViewController : UITableViewController
+
+@end
