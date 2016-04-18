37f3d399e58467dd4c78a39b2925ddfaecb60f25
aabato <angelica.bato@gmail.com>
2016-04-18 19:07:53 -0400

    update fonts~

---
+++ FanBase/FanBase/UILabel+SubstituteFont.h
@@ -0,0 +1,15 @@
+//
+//  UILabel+SubstituteFont.h
+//  LazyProgrammersGuideToDecentUI
+//
+//  Created by Timothy Clem on 4/12/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (SubstituteFont)
+
+@property (nonatomic, copy) NSString *fis_substituteFontName UI_APPEARANCE_SELECTOR;
+
+@end
