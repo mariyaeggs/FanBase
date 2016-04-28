2db450685f6371f19d33f27f6c17b2c8be3f732e
mariyaeggs <mariyawilson@att.net>
2016-04-28 16:38:56 -0400

    Adds EULA agreement

---
+++ FanBase/FanBase/EULAViewController.m
@@ -0,0 +1,28 @@
+//
+//  EULAViewController.m
+//  FanBase
+//
+//  Created by Mariya Eggensperger on 4/28/16.
+//  Copyright © 2016 Angelica Bato. All rights reserved.
+//
+
+#import "EULAViewController.h"
+
+@implementation EULAViewController
+
+
+-(void) viewDidAppear:(BOOL)animated {
+    
+    [super viewDidAppear:animated];
+    
+    // Edit any UITextView Content here programmatically and set constraints if necessary
+
+}
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+}
+
+@end
